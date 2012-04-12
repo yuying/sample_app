00d7c4f3ee818500820aafb1616b6d9083c9fef2
yuying <yuying1@umbc.edu>
2012-04-12 02:08:33 -0400

    Done with ch9 exercise 1

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -35,17 +35,5 @@ describe "Users" do
       end
     end
 
-    describe "success" do
-      it "should sign a suer in and out" do
-        user = Factory(:user)
-        visit signin_path
-        fill_in :email,     :with => user.email
-        fill_in :password,  :with => user.password
-        click_button
-        controller.should be_signed_in
-        click_link "Sign out"
-        controller.should_not be_signed_in
-      end
-    end
   end
 end
