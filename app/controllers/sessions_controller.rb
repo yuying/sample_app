7672b83beea83272f19294b6d77dacf3fd022ee0
yuying <yuying1@umbc.edu>
2012-04-19 02:39:40 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,4 +21,18 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end
+
+  def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination."
+      @title = "Sign in"
+      render 'new'
+    else
+      sign_in user
+      redirect_back_or user
+    end
+  end
+
 end
