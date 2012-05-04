4c16e52c0bb7b6709058389a1462d05cac6ac48b
yuying <yuying1@umbc.edu>
2012-05-04 02:50:53 -0400

    Added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
 
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -36,9 +43,20 @@ class User < ActiveRecord::Base
     (user && user.salt == cookie_salt) ? user : nil
   end
 
-  def feed
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
 
-    Micropost.where("user_id = ?", id)
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
+
+  def feed
+    Micropost.from_users_followed_by(self)
   end
 
   private
